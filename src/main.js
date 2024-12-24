d8063ba1957309a6b258cf5f7f242fb3c8c1f109
tianzhaohui <tianzhaohui1109@gmail.com>
2024-12-24 16:18:16 +0800

    添加依赖

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+// 引入 Element Plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 如果需要使用中文
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(store)
+   .use(router)
+   .use(pinia)
+   .use(ElementPlus, {
+     locale: zhCn,
+   })
+   .mount('#app')
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,1 +1,40 @@
- 
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+// 创建 axios 实例
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API || '',
+  timeout: 15000
+})
+
+// 请求拦截器
+service.interceptors.request.use(
+  config => {
+    // 可以在这里添加token等认证信息
+    return config
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    // 这里可以根据后端返回的状态码做相应的处理
+    return res
+  },
+  error => {
+    console.log('err' + error)
+    ElMessage({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service 
